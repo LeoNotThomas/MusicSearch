02deebe0b5ba995c8e9ab9c63554a80dc614f5a5
Thomas Leonhardt <t.leonhardt@gmx.net>
2022-04-03 14:19:08 +0200

    Initial Commit

---
+++ MusicSearch/MusicSearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicSearchApp.swift
+//  MusicSearch
+//
+//  Created by Thomas (privat) Leonhardt on 03.04.22.
+//
+
+import SwiftUI
+
+@main
+struct MusicSearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
